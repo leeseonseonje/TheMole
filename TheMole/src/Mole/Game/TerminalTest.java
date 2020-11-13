71934ecd4aa05f13dd0b25b55ebd2ff38096c4da
seon <leeseonje9323@gmail.com>
2020-11-13 23:01:35 +0900

    delete

--- TheMole/src/Mole/Game/TerminalTest.java
+++
@@ -1,7 +0,0 @@
-package Mole.Game;
-
-public class TerminalTest {
-	public static void main(String[] args) {
-		System.out.println("Zz");
-	}
-}
--- TheMole/src/Mole/Game/Testttt.java
+++
@@ -1,9 +0,0 @@
-package Mole.Game;
-
-public class Testttt {
-	public static void main(String[] args) {
-		System.out.println("ddasdsadasdsadasdasdsadasdasd");
-		System.out.println("ddfsfasdfasdfasdfasdfasdfasd");
-		System.out.println("onetwothree");
-	}
-}

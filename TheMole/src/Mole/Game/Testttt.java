def4057c5edbeba9d0c0fda4a7eab64140db8055
dudwn <dudwn@DESKTOP-6JG5UKN>
2020-11-13 21:16:54 +0900

    Test

--- TheMole/src/Mole/Game/Testttt.java
+++ TheMole/src/Mole/Game/Testttt.java
@@ -3,5 +3,6 @@ package Mole.Game;
 public class Testttt {
 	public static void main(String[] args) {
 		System.out.println("ddasdsadasdsadasdasdsadasdasd");
+		System.out.println("ddfsfasdfasdfasdfasdfasdfasd");
 	}
 }

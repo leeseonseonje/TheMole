82fa21259fb0c061b68ecc4bf0429257337575f5
조승완 <wanni4526@gmail.com>
2020-11-14 20:54:56 +0900

    game methods write( run, start, stop )

--- TheMole/src/Mole/Game/Game.java
+++ TheMole/src/Mole/Game/Game.java
@@ -1,23 +1,62 @@
 package Mole.Game;
 
 import java.awt.Canvas;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 
+	private static final long serialVersionUID = 1L;
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = WIDTH / 12 * 9;
-	public static final int SCALE = 2;
+	public static final int SCALE = 1;
 	public final String TITLE = "Mole Game";
 	
-	public Game() {
+	private boolean running = false;
+	private Thread thread;
+	
+	private synchronized void start() {
+		if(running)
+			return;
 		
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	private synchronized void stop() {
+		if(!running)
+			return;
+		
+		running = false;
+		try {
+			thread.join(); // 모든 스레드가 끝날때까지 기다렸다가 다음(System.exit(1))으로 실행
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.exit(1);
 	}
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() { // Runnable의 기본으로 구현된 메소드
+		while(running) {
+			// Game Loop
+		}
+	}
+	public static void main(String args[]) {
+		Game game = new Game();
 		
+		game.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT * SCALE));
+		game.setMaximumSize(new Dimension(WIDTH*SCALE, HEIGHT * SCALE));
+		game.setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT * SCALE));
+	
+		JFrame frame = new JFrame(game.TITLE);
+		frame.add(game);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 }

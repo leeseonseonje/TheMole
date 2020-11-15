be7a893fddbdd8f9c2128552656ce212f259ea73
leeseonseonje <leeseonje9323@gamail.com>
2020-11-15 16:59:24 +0900

    a

--- TheMole/src/MoleServer/MainServer.java
+++ TheMole/src/MoleServer/MainServer.java
@@ -1,5 +1,24 @@
 package MoleServer;
 
-public class MainServer {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+public class MainServer {
+	private static final int PORT = 10002;
+	
+	public static void main(String[] args) {
+	}
+	
+	public void serverStart() throws Exception {
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap serverbootstrap = new ServerBootstrap();
+			serverbootstrap.group(bossGroup, workerGroup)
+			.channel(NioServerSocketChannel.class)
+			.childHandler(new MoleServerInitializer());
+		} catch (Exception e) {}
+	}
 }

e045413af360e69f2afd2785ed4775129a0f5020
jaeuk123 <dnrdlrjt@naver.com>
2020-11-23 19:43:22 +0900

    작물 재성성 완성

--- TheMole/src/Mole/Game/MoleUI.java
+++ TheMole/src/Mole/Game/MoleUI.java
@@ -2,6 +2,8 @@ package Mole.Game;
 
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -19,6 +21,7 @@ import javax.swing.Timer;
 class MoleUII extends JFrame {
 	private MolePanel molePanel;
 
+
 	public MoleUII() throws IOException, InterruptedException { // Mole UI 창
 		setTitle("Mole Game");
 		setSize(800, 600);
@@ -27,7 +30,7 @@ class MoleUII extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		molePanel = new MolePanel(); // molePanel 생성
-
+		
 		add(molePanel);
 		setVisible(true);
 	}
@@ -94,12 +97,15 @@ class MolePanel extends JPanel {
 
 	
 
-	class vegetableThread extends Thread  {
+	class vegetableThread {
 		private ImageIcon veget = new ImageIcon("img/vegetables.png");
 		JLabel vegetable = new JLabel(veget);
-		int x, y,section;
-		Timer timer ;
-
+		private int x, y,section;
+		private Timer vegtimer;
+		private int vegsecond;
+		private int vegcount = 0;
+		private boolean timerstop = false;
+		
 		
 
 		public vegetableThread(int section) {
@@ -118,30 +124,41 @@ class MolePanel extends JPanel {
 		public int getY() {
 			return y;
 		}
-		
+		public void setsecond(int second) {
+			vegsecond = second;
+		}
+		public int getsecond() {
+			return vegsecond;
+		}
+		public int getcouont() {
+			return vegcount;
+		}
+		public int setcount(int count) {
+			return vegcount = count;
+		}
 		
 		public void setposition() {
 			x = ((int) (Math.random() * 260)) + 263 * this.section;
 			y = 260;
-			vegetable.setBounds(x, y, 16, 16);
-			 
+			vegetable.setBounds(x, y, 16, 16);			 
 		}
-
-		@SuppressWarnings("static-access")
-		@Override
-		public void run() {
-			try {
-				vegetable.setVisible(false);
-				vegetableThread.sleep(5000);
-				setposition();
-				vegetable.setVisible(true);
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		public void vegtimer() {
+			timerstop = true;
+			vegtimer = new Timer(1000,new ActionListener(){
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					vegsecond++;
+					if(vegsecond == 10) {
+						setposition();
+						vegetable.setVisible(true);
+						vegtimer.stop();
+						timerstop = false;
+						vegcount++;
+					}
+				}
+			});
 		}
-
+		
 		public void setVisible(boolean b) {
 			if (b == true)
 				vegetable.setVisible(true);
@@ -156,12 +173,14 @@ class MolePanel extends JPanel {
 		private Rectangle champion;
 		private ImageIcon mole = new ImageIcon("img/moleimg.png");
 		private ImageIcon moleSelect = new ImageIcon("img/moleselect.png");
-		Thread t1 = new Thread();
-
 
 		private Timer timer;
 		private double speed = 0.15;
 		private Long startTime;
+		
+		private Timer eattimer;
+		private int eatsecond;
+		private boolean eating = false;
 
 		private double targetX, targetY;
 		private double startX, startY;
@@ -189,12 +208,14 @@ class MolePanel extends JPanel {
 			add(moleButton);
 
 			moleButton.addActionListener(e -> {
-				if (e.getSource() == moleButton) {
+				if (e.getSource() == moleButton &&eating == false) {
 					moleButton.setIcon(moleSelect);
 					addMouseListener(new MouseAdapter() {
 						@Override
 						public void mousePressed(MouseEvent e) {
-							if (e.getButton() == MouseEvent.BUTTON3) {
+							if (e.getButton() == MouseEvent.BUTTON3 ) {
+								if(eating == true)
+									moleButton.setIcon(mole);
 								if (moleButton.getIcon().equals(moleSelect)) {
 									timer.stop();
 									calculateChampionMovement(e.getX(), e.getY(), champion);
@@ -210,19 +231,32 @@ class MolePanel extends JPanel {
 						}
 					});
 				}
-
 			});
 			timer = new Timer(10, e -> {
-				TimeMove();
+				if(eating == false) {
+					TimeMove();
+				}else 
+					timer.stop();
 			});
-
 		}
+		public void eatTimer() {
+			eating = true;
+			eattimer = new Timer(1000,new ActionListener(){	
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					eatsecond++;
+					if(eatsecond == 3) {
+						eattimer.stop();
+						eating = false;
+					}
+				}
+			});
+		} 
 
 		public void run() {
 			
-
 		}
-
+		
 		public void TimeMove() {
 			long duration = System.currentTimeMillis() - startTime;
 			double progress = duration / runTime;
@@ -240,18 +274,31 @@ class MolePanel extends JPanel {
 				moleButton.setBounds((int) x - 15, (int) y - 15, 30, 30);
 				champion.setRect(x - 5, y - 5, 10, 10);
 			}
-			if (v0.getX() == x && v0.getY() >= y - 15) {
-				v0.run();
-			} else if (v1.getX() == x && v1.getY() >= y - 15) {
+			if (v0.getX() == x && v0.getY() >= y - 15 &&v0.timerstop == false && eating == false) {
+				v0.setVisible(false);
+				v0.setsecond(0);
+				v0.vegtimer();
+				v0.vegtimer.start();
+				eatsecond = 0;
+				eatTimer();
+				eattimer.start();
+			} else if (v1.getX() == x && v1.getY() >= y - 15 &&v1.timerstop == false && eating == false) {
 				v1.setVisible(false);
-				v1.setposition();
-				v1.setVisible(true);
-			} else if (v2.getX() == x && v2.getY() >= y - 15) {
+				v1.setsecond(0);
+				v1.vegtimer();
+				v1.vegtimer.start();
+				eatsecond = 0;
+				eatTimer();
+				eattimer.start();
+			} else if (v2.getX() == x && v2.getY() >= y - 15 &&v2.timerstop == false && eating == false) {
 				v2.setVisible(false);
-				v2.setposition();
-				v2.setVisible(true);
+				v2.setsecond(0);
+				v2.vegtimer();
+				v2.vegtimer.start();
+				eatsecond = 0;
+				eatTimer();
+				eattimer.start();
 			}
-
 		}
 
 		public void calculateChampionMovement(double x, double y, Rectangle champion) {
@@ -263,6 +310,18 @@ class MolePanel extends JPanel {
 
 				startX = champion.getCenterX();
 				startY = champion.getCenterY();
+				if(targetX - startX > 0) {
+					if(targetY - startY > 0) {
+							System.out.println("4사분면");
+					}else 
+						System.out.println("1사분면");
+				} else if (targetX-startX < 0) {
+					if(targetY - startY > 0) {
+						System.out.println("3사분면");
+				}else 
+					System.out.println("2사분면");
+				}
+				
 				double distance = Math
 						.sqrt((startX - targetX) * (startX - targetX) + (startY - targetY) * (startY - targetY));
 

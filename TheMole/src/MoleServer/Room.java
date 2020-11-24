86e655b934a77f83c48e30552b0f55c2a2629316
leeseonseonje <leeseonje9323@gamail.com>
2020-11-24 17:40:51 +0900

    3

--- TheMole/src/MoleServer/Room.java
+++ TheMole/src/MoleServer/Room.java
@@ -1,8 +1,9 @@
 package MoleServer;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.Map.Entry;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
@@ -13,82 +14,82 @@ import io.netty.util.concurrent.GlobalEventExecutor;
 public class Room {
 	public static final LinkedList<ChannelGroup> roomList = new LinkedList<ChannelGroup>();
 	private static final LinkedList<String> roomHostUser = new LinkedList<String>();
+	public static final HashMap<String, ChannelGroup> roomManager = new HashMap<String, ChannelGroup>();
 
 	public static void roomCreat(ChannelHandlerContext ctx, String roomHost) {
-		Channel Host = ctx.channel();
+		Channel host = ctx.channel();
 		ChannelGroup room = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
-		room.add(Host);
-		roomList.add(room);
-		roomHostUser.add(roomHost);
+	
+		room.add(host);
+		roomManager.put(roomHost, room);
 		ctx.writeAndFlush("CREAT," + roomHost);
 	}
 
 	public static void roomJoin(ChannelHandlerContext ctx, String roomHostName, String myName) {
 		Channel guest = ctx.channel();
-		int index = Collections.binarySearch(roomHostUser, roomHostName);
-		if (roomList.get(index).size() > 1) 
+		
+		if (roomManager.get(roomHostName).size() > 1)
 			ctx.writeAndFlush("FULL");
 		else {
-		roomList.get(index).add(guest);
-		for (Channel channel : roomList.get(index)) {
-			if(channel == guest)
-				guest.writeAndFlush("JOIN," + roomHostName + "," + myName);
-			else
-				channel.writeAndFlush("GUEST," + roomHostName + "," + myName);
+			roomManager.get(roomHostName).add(guest);
+			for (Channel channel : roomManager.get(roomHostName)) {
+				if(channel == guest)
+					guest.writeAndFlush("JOIN," + roomHostName + "," + myName);
+				else
+					channel.writeAndFlush("GUEST," + roomHostName + "," + myName);
 			}
 		}
 	}
 	
 	public static void roomListSend(ChannelHandlerContext ctx) {
 		ctx.write("ROOMLIST,");
-		for(int i = 0; i < roomHostUser.size(); i++) 
-			ctx.write(roomHostUser.get(i) + ",");
+		for (Entry<String, ChannelGroup> entry : roomManager.entrySet())
+			ctx.write(entry.getKey() + ",");
 		ctx.flush();
 	}
-	public static void roomListRefresh(ChannelHandlerContext ctx) {
+	public static void roomListRefresh(ChannelHandlerContext ctx) {		
 		ctx.write("REFRESH,");
-		for(int i = 0; i < roomHostUser.size(); i++) 
-			ctx.write(roomHostUser.get(i) + ",");
+		for (Entry<String, ChannelGroup> entry : roomManager.entrySet())
+			ctx.write(entry.getKey() + ",");
 		ctx.flush();
 	}
 	public static void roomDelete(ChannelHandlerContext ctx, String hostName) {
 		Channel me = ctx.channel();
-		int index = Collections.binarySearch(roomHostUser, hostName);
-		roomHostUser.remove(hostName);
-		for (Channel channel : roomList.get(index)) {
+		
+		for (Channel channel : roomManager.get(hostName)) {
 			if(channel == me) {
 				me.write("REFRESH,");
-				for(int i = 0; i < roomHostUser.size(); i++) 
-					me.write(roomHostUser.get(i) + ",");
+				for (Entry<String, ChannelGroup> entry : roomManager.entrySet())
+					me.write(entry.getKey() + ",");
 				me.flush();
 			} else {
 				channel.write("BOOM,");
-				for(int i = 0; i < roomHostUser.size(); i++) 
-					channel.write(roomHostUser.get(i) + ",");
-				channel.flush();
+				for (Entry<String, ChannelGroup> entry : roomManager.entrySet())
+					me.write(entry.getKey() + ",");
+				me.flush();
 			}
 		}
-		roomList.remove(index);
+		roomManager.remove(hostName);
 	}
 	public static void roomOut(ChannelHandlerContext ctx, String hostName) {
 		Channel guest = ctx.channel();
-		int index = Collections.binarySearch(roomHostUser, hostName);
-		roomList.get(index).remove(guest);
+
+		roomManager.get(hostName).remove(guest);
 		ctx.write("REFRESH,");
-		for(int i = 0; i < roomHostUser.size(); i++) 
-			ctx.write(roomHostUser.get(i) + ",");
+		for (Entry<String, ChannelGroup> entry : roomManager.entrySet())
+			ctx.write(entry.getKey() + ",");
 		ctx.flush();
 	}
 	public static void readyState(ChannelHandlerContext ctx, String m, String hostName) {
 		Channel guest = ctx.channel();
-		int index = Collections.binarySearch(roomHostUser, hostName);
-		if (m.equals("[READY]")) { 
-			for (Channel channel : roomList.get(index)) {
+		
+		if (m.equals("[READY]")) {
+			for (Channel channel : roomManager.get(hostName)) {
 				if(channel != guest)
 					channel.writeAndFlush("READY");
 			}
-		} else { 
-			for (Channel channel : roomList.get(index)) {
+		} else {
+			for (Channel channel : roomManager.get(hostName)) {
 				if(channel != guest)
 					channel.writeAndFlush("CANSLE");
 			}
@@ -96,20 +97,20 @@ public class Room {
 	}
 	public static void roomChatting(ChannelHandlerContext ctx, String message, String name, String hostName) {
 		Channel host = ctx.channel();
-		int index = Collections.binarySearch(roomHostUser, hostName);
-		for (Channel channel : roomList.get(index)) {
+	
+		for (Channel channel : roomManager.get(hostName))
 			channel.writeAndFlush("SENDMESSAGE," + name + ": " + message);
-		}
 	}
 	public static void startGame(ChannelHandlerContext ctx, String hostName) {
 		Channel host = ctx.channel();
 		int index = Collections.binarySearch(roomHostUser, hostName);
 		int r = (int)(Math.random()*2);
-		for (Channel channel : roomList.get(index)) {
+		
+		for (Channel channel : roomManager.get(hostName)) {
 			if (r == 0 && channel != host) {
 				host.writeAndFlush("MOLESTART");
 				channel.writeAndFlush("HUMANSTART");
-			} 
+			}
 			else if (r == 1 && channel != host) {
 				host.writeAndFlush("HUMANSTART");
 				channel.writeAndFlush("MOLESTART");

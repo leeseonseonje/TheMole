b4bc75612503693f2f0c34d9a59f62d6843e2a04
조승완 <wanni4526@gmail.com>
2021-08-06 02:07:41 +0900

    delete useless file

--- TheMole/src/Mole/Game/Physics.java
+++ TheMole/src/Mole/Game/Physics.java
@@ -7,7 +7,7 @@ import Mole.Game.Entities.EntityB;
 
 public class Physics { // 충돌감지, 사각형의 범위안에 있으면 true로 반환 없으면 false
 
-	public static boolean Collision(EntityA entA, LinkedList<Mole> entB) { // 인간 대 두더지N , 총알1개와 두더지 1하나의 관계
+	public static boolean Collision(EntityA entA, LinkedList<Mole> entB) { // 인간 대 두더지 N , 총알1개와 두더지 1하나의 관계
 		for (int i = 0; i < entB.size(); i++)
 			if (entA.getBounds().intersects(entB.get(i).getBounds())) {
 				entB.remove(i);
@@ -22,13 +22,4 @@ public class Physics { // 충돌감지, 사각형의 범위안에 있으면 true로 반환 없으면 
 		}
 		return false;
 	}
-	/*
-	public static boolean Collision(LinkedList<EntityA> entA, LinkedList<Mole> entB) { // 총알N 대 두더지N
-		for (int i = 0; i < entA.size(); i++)
-			for (int j = 0; j < entB.size(); j++)
-				if ((entA.get(i).getBounds()).intersects(entB.get(j).getBounds()))
-					return true;
-		return false;
-	}
-	*/
 }
